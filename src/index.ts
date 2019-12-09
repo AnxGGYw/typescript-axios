b9956db6b339f08344878688ce219139ae7ad79c
cggcbb <449567533@qq.com>
2019-12-09 21:15:01 +0800

    feat(index.ts): processConfig

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import { RequestConfig } from './types'
 import xhr from './xhr'
+import { buildURL } from './helpers/url'
 
+// 入口entry
 const axois = (config: RequestConfig): void => {
+  processConfig(config)
   xhr(config)
+}
+// 处理配置
+const processConfig = (config: RequestConfig): void => {
+  config.url = transformURL(config)
+}
+// 转换url
+const transformURL = (config: RequestConfig): string => {
+  const { url, params } = config
+  return buildURL(url, params)
 }
 
 export default axois
